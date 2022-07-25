28c8ee1efc118450ac2fb53c4c6f5c82a0d99625
Joel <jdharri@gmail.com>
2022-07-25 17:18:56 -0400

    Update fastapi_pika_client.py

--- fastapi_pika_client.py
+++ fastapi_pika_client.py
@@ -1,1 +1,19 @@
+import json
+import aio_pika as pika
+from aio_pika import connect, connect_robust, IncommingMessage, ExchangeType, Message, Exchange
+import uuid
+import ssl
+from os import getenv as env
 
+RMQ_HOST = env('RMQ_HOST')
+RMQ_PORT = env('RMQ_PORT')
+RMQ_VHOST = env('RMQ_VHOST')
+RMQ_USERNAME = env('RMQ_USERNAME', None)
+RMQ_PASSWORD = env('RMQ_PASSWORD', None)
+
+
+class PikaClient:
+  
+  def __init__(self, callback, host=RMQ_HOST, port=RMQ_PORT, vhost=RMQ_VHOST, ssl_enabled=RMQ_SSL, **kwargs):
+    
+    self.host = host
